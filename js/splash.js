49c6e7ce93e1c289b79186a1ec95a87885bf89e5
Wasteland-rose <spooky_abyss@au.com>
2025-04-22 00:24:56 +0900

    Add files via upload

--- js/splash.js
+++ js/splash.js
@@ -1,20 +1,24 @@
 // splash.js
 
 $(document).ready(function() {
+  // 初回アクセス時
   if (!sessionStorage.getItem('splashShown')) {
+    // 黒背景（#splash）からページ内容にフェードイン
     $('#splash').fadeOut(1000, function() {
-      $(this).css('display', 'none');
+      $(this).css('display', 'none'); // フェードアウト後に非表示にする
     });
+    // sessionStorageにアクセス済みを保存
     sessionStorage.setItem('splashShown', 'true');
   } else {
+    // 二回目以降は何もしない、すぐに表示
     $('#splash').css('display', 'none');
   }
-});
 
-if (window.innerWidth <= 599) {
-	const favicon = document.querySelector('link[rel="icon"]');
-		if (favicon) {
-			favicon.remove();
-		}
-	}
+  // スマホ画面サイズならfaviconを非表示
+  if (window.innerWidth <= 599) {
+    const favicon = document.querySelector('link[rel="icon"]');
+    if (favicon) {
+      favicon.remove(); // faviconを非表示にするために削除
+    }
+  }
 });
