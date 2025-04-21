0562fef422239a9585977bc068dd5c3d84fbd70b
Wasteland-rose <spooky_abyss@au.com>
2025-04-21 23:33:23 +0900

    Add files via upload

---
+++ js/splash.js
@@ -0,0 +1,16 @@
+// splash.js
+
+$(document).ready(function() {
+  // 初回アクセス時
+  if (!sessionStorage.getItem('splashShown')) {
+    // 黒背景（#splash）からページ内容にフェードイン
+    $('#splash').fadeOut(1000, function() {
+      $(this).css('display', 'none'); // フェードアウト後に非表示にする
+    });
+    // sessionStorageにアクセス済みを保存
+    sessionStorage.setItem('splashShown', 'true');
+  } else {
+    // 二回目以降は何もしない、すぐに表示
+    $('#splash').css('display', 'none');
+  }
+});
