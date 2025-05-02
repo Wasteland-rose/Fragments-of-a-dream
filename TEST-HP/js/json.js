5eca8200ecc7b9043559c1b57c9c581587500606
Wasteland-rose <spooky_abyss@au.com>
2025-05-02 15:33:52 +0900

    Add files via upload

--- TEST-HP/js/json.js
+++ TEST-HP/js/json.js
@@ -8,17 +8,18 @@ fetch('json/blackout.json')
 			document.body.style.backgroundColor = 'black';
 			
 			setTimeout(() => {
+				// ホワイトアウト処理
 				document.body.style.transition = `background-color ${data.loginScene.brightScene.fadeInDuration}ms ${data.loginScene.brightScene.easing}`;
 				document.body.style.backgroundColor = 'white'; // 明るくなる演出
+
+				// HPのフェードイン
+				setTimeout(() => {
+					const mainContent = document.getElementById('Main-Content');
+					mainContent.style.transition = `opacity ${data.loginScene.brightScene.fadeInDuration}ms ${data.loginScene.brightScene.easing}`;
+					mainContent.style.opacity = 1; // フェードインさせる
+				}, data.loginScene.brightScene.fadeInDuration); // ホワイトアウト終了後にHP表示
 			}, data.loginScene.transition.delay);
 		}
-
-		// HPのフェードイン
-		setTimeout(() => {
-			const mainContent = document.getElementById('Main-Content');
-			mainContent.style.transition = `opacity ${data.loginScene.brightScene.fadeInDuration}ms ${data.loginScene.brightScene.easing}`;
-			mainContent.style.opacity = 1; // フェードインさせる
-		}, data.loginScene.transition.delay);
 	})
 	.catch(error => {
 		console.error('JSONの読み込みエラー:', error);
