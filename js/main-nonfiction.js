318b6918fcfbd8a090000a147796affe97659c47
Wasteland-rose <spooky_abyss@au.com>
2025-05-03 11:26:13 +0900

    Add files via upload

---
+++ js/main-nonfiction.js
@@ -0,0 +1,12 @@
+//script.js
+
+$("#nonfiction-content-decoartion-menu").html(
+	'<ul id="ContentMenuTitle">' +
+		'<li><a href="nonfiction/1.html">カラオケボックス</a></li>' +
+		'<li><a href="nonfiction/2.html">四角い窓</a></li>' +
+		'<li><a href="nonfiction/3.html">立ち入り禁止</a></li>' +
+		'<li><a href="nonfiction/4.html">夕暮れ時の部屋</a></li>' +
+		'<li><a href="nonfiction/5.html">1:37</a></li>' +
+		'<li><a href="nonfiction/6.html">「次は無いよ」</a></li>' +
+	'</ul>'
+);
