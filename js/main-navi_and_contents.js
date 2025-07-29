85a2ecffdfe72edafe94f6b96b4923319e8c7682
Wasteland-rose <spooky_abyss@au.com>
2025-07-29 19:34:31 +0900

    Add files via upload

--- js/main-navi_and_contents.js
+++ js/main-navi_and_contents.js
@@ -36,12 +36,22 @@ $(document).ready(function () {
     $("#TextContents").html(
 
         '<div class="search">' +
-            '<div class="content ShortStory R15 Horror Serious Flow" data-date="2025-07-28">' +
+            '<div class="content LongStory AllAges Love Flow" data-date="2025-07-28">' +
+            '<div class="O">' +
+            '<div class="Title_O"><a href="#">微笑む君に花束を</a></div>' +
+	            '<div class="Tag_O">長編 / 全年齢 / 恋愛 / ノリ</div>' +
+	            '<div class="Summary_O">執筆中</div>' +
+	            '<div class="Writing_O">執筆日：執筆中</div>' +
+            '<div class="Plot_O"><a href="#">あとがき</a></div>' +
+            '</div></div></div>' +
+
+        '<div class="search">' +
+            '<div class="content LongStory R15 Horror Serious Flow" data-date="2025-07-28">' +
             '<div class="Pu">' +
             '<div class="Title_Pu"><a href="main/Ridicule.html">Ridicule</a></div>' +
-	            '<div class="Tag_Pu">短編 / R15 / ホラー / シリアス / ノリ</div>' +
+	            '<div class="Tag_Pu">長編 / R15 / ホラー / シリアス / ノリ</div>' +
 	            '<div class="Summary_Pu">厳格な規則と謎に満ちた学園に売られた妹・ニアが、兄ノアとともに狂気と恐怖の中で運命を切り開く。読者参加型のダークファンタジー物語。<BR />分岐エンド有</div>' +
-	            '<div class="Writing_Pu">執筆日：執筆中</div>' +
+	            '<div class="Writing_Pu">執筆日：2025/07/28</div>' +
             '<div class="Plot_Pu"><a href="main/plot/Ridicule.html">あとがき</a></div>' +
             '</div></div></div>' +
 
